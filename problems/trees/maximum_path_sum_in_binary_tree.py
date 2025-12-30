6b6b2d81e1b189b827276af8f6a24a5a69c998a6
Muhammad Furqan <143472071+furqanmirzaa@users.noreply.github.com>
2025-12-30 12:42:44 +0500

    feat: Initial problem statement and skeleton code

---
+++ problems/trees/maximum_path_sum_in_binary_tree.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        """
+        Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+        You must write an algorithm that runs in O(n) time.
+        """
+        # TODO: Implement the solution
+        return 0
